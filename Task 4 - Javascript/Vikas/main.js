bd2646da09d8d3ef09724f7e29938cc187d81874
Vikas Mukkamla <vikasmukkamla@gmail.com>
2023-08-07 14:27:47 +0530

    first

--- Task 4 - Javascript/Vikas/main.js
+++ Task 4 - Javascript/Vikas/main.js
@@ -3,7 +3,10 @@ function showStore(products) {
     for (let i = 0; i < products.length; i++) {
       productTag += `<div class="col-6 col-md-3">
                             <div class="card">
-                                <img src="${products[i].image}" class="card-img-top" alt="Iphone">
+                               <div>
+                               <div class="float-end m-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                               <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                                </svg></div><img src="${products[i].image}" class="card-img-top" alt="Iphone"></div> 
                                 <div class="card-body">
                                     <p class="card-title">${products[i].title}</p>
                                     <h3>₹ ${products[i].price}.00</h3>
